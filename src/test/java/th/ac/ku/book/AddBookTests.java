03977a0282ea1e065cbd560cc1cad23398f894aa
ssunny-mengg <khemmaporn.n@ku.th>
2022-09-14 20:37:36 +0700

    lab11

---
+++ src/test/java/th/ac/ku/book/AddBookTests.java
@@ -0,0 +1,46 @@
+package th.ac.ku.book;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AddBookTests {
+
+    @LocalServerPort
+    private Integer port;
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 1000);
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driver.get("http://localhost:" + port + "/book/add");
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+}
+
